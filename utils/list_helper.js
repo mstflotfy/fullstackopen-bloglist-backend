3301e4cc8d74f707fd9b0b29336dfb4acef107fc
mstflotfy <mstf.lotfy@gmail.com>
2024-04-22 00:11:07 +0200

    submit 4.5*: Helper Functions and Unit Tests, step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,15 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sum, x) => sum + x.likes, 0);
 
+const favoriteBlog = (blogs) => blogs.reduce((acc, x) => {
+  if ((x.likes > 0 && acc.likes === undefined) || (x.likes > acc.likes)) {
+    return x;
+  }
+  return acc;
+}, {});
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
