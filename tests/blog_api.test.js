49d68de162dfd6923e4e96f136d4baea7cf97d81
mstflotfy <mstf.lotfy@gmail.com>
2024-05-02 18:25:07 +0300

    submit 4.10: Blog List Tests, step 3

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -26,9 +26,28 @@ test('blogs are returned as json & include the same number of init blogs', async
   assert.strictEqual(response.body.length, helper.initialBlogs.length);
 });
 
-test.only('blog unique identifier is id not _id', async () => {
+test('blog unique identifier is id not _id', async () => {
   const response = await api.get('/api/blogs');
   assert(response.body[0].id);
+});
+
+test.only('A valid blog can be added', async () => {
+  const newBlog = {
+    title: 'post request works',
+    author: 'mstflotfy',
+    url: '/test-post',
+    likes: 1,
+  };
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('content-type', /application\/json/);
+  const blogsAfter = await helper.blogsInDb();
+  assert.strictEqual(blogsAfter.length, helper.initialBlogs.length + 1);
+
+  const blogsTitles = blogsAfter.map((b) => b.title);
+  assert(blogsTitles.includes(newBlog.title));
 });
 
 after(async () => {
