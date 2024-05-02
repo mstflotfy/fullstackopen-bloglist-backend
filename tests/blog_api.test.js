1f93820ea39bce69fadc5a8bc9562918f08ae87f
mstflotfy <mstf.lotfy@gmail.com>
2024-05-02 16:31:48 +0300

    submit 4.9: Blog List Tests, step 2

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -17,13 +17,18 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-test.only('blogs are returned as json & includes the same number of init blogs', async () => {
+test('blogs are returned as json & include the same number of init blogs', async () => {
   const response = await api
     .get('/api/blogs')
     .expect(200)
     .expect('content-type', /application\/json/);
 
   assert.strictEqual(response.body.length, helper.initialBlogs.length);
+});
+
+test.only('blog unique identifier is id not _id', async () => {
+  const response = await api.get('/api/blogs');
+  assert(response.body[0].id);
 });
 
 after(async () => {
