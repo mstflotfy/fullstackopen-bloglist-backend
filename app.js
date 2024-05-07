48c650889240606f94f144937fe3c0908670a9e9
mstflotfy <mstf.lotfy@gmail.com>
2024-05-07 22:58:37 +0300

    submit 4.15: Blog List Expansion, step 3

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users')
 const { MONGODB_URI } = require('./utils/config');
 
 mongoose.set('strictQuery', false);
@@ -17,5 +18,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
--- models/blog.js
+++ models/blog.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
+/*
+  In stark contrast to the conventions of relational databases,
+  references are now stored in both documents: the blog references the user who created it,
+  and the user has an array of references to all of the blogs created by them.
+*/
+
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 blogSchema.set('toJSON', {
