024f34f762515fea41ea3b03006f2c84f4c42c70
mstflotfy <mstf.lotfy@gmail.com>
2024-05-12 00:21:12 +0300

    submit 4.18: Blog List Expansion, step 6

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const middleware = require('./utils/middleware');
 const blogsRouter = require('./controllers/blogs');
-const usersRouter = require('./controllers/users')
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const { MONGODB_URI } = require('./utils/config');
 
 mongoose.set('strictQuery', false);
@@ -18,6 +19,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/login', loginRouter);
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use(middleware.errorHandler);
