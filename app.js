462f706e04fd0be27e2818e75c880d6f3f1af3f6
mstflotfy <mstf.lotfy@gmail.com>
2024-07-01 09:35:51 +0300

    submit: 5.18: Blog List End To End Testing, step 2

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const middleware = require('./utils/middleware');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const testingRouter = require('./controllers/testing');
 const { MONGODB_URI } = require('./utils/config');
 
 mongoose.set('strictQuery', false);
@@ -20,9 +21,14 @@ app.use(cors());
 app.use(express.json());
 
 app.use(middleware.tokenExtractor);
+
+if (process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter);
+}
 app.use('/api/login', loginRouter);
 app.use('/api/blogs', middleware.userExtractor, blogsRouter);
 app.use('/api/users', usersRouter);
+
 app.use(middleware.errorHandler);
 
 module.exports = app;
